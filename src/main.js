fd45af51917c10ac534df573ba5ba0c07e18c0f3
Rudra0Panda <rudrabroker@gmail.com>
2025-06-07 11:35:26 +0530

    git

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createGrid , createHorizontalLines , createLines } from "./paper"; 
+import EditorJS from '@editorjs/editorjs';
 //createHorizontalLines( 35, '#ffffff', 0.1) 
 //createLines(50,'rgb(150, 150, 150)',0.2);
+
+
+// Inside your script tag or frontend JS/TS file
+
+let mousePos = {
+  mouseX: 0,
+  mouseY: 0
+};
+
+document.addEventListener('mousemove', (e) => {
+  mousePos.mouseX = e.clientX;
+  mousePos.mouseY = e.clientY;
+});
+
+
 createGrid(50,1,'rgb(150, 150, 150)',0.2);
+
+createTextBlock = ({
+  
+}) 
+
+TextDiv = {
+ tag:"div",
+ content:"",
+ id:`${Math.random()}`
+const editor = new EditorJS({
+  holder:'element'
+});
